845f9e13fcdc62ec39a0c452d4501968ddda5b7f
Emil Roydev <125214785+ERoydev@users.noreply.github.com>
2023-10-02 10:38:38 -0700

    Add files via upload

---
+++ Function and Statements - Exercise/Smallest of Three Numbers.js
@@ -0,0 +1,12 @@
+
+function solve(n1, n2, n3) {
+    let smallestNumber = function () {
+        return Math.min(n1, n2, n3)
+    }
+
+    console.log(smallestNumber())
+}
+
+solve(600, 342, 123)
+
+
